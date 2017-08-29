1cdee5f840f1c4d558d885db5470d06c8626093e
adiu12 <30682561+adiu12@users.noreply.github.com>
2017-08-29 18:19:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'aditmcet',
+    database:'aditmcet',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
+var pool=new Pool(config);
+pool.query('SELECT * from test',function(err,result){
+    app.get('/',test-db,function(req,res){
+    if(err)
+        res.status(500).send(err.toString());
+        else
+        res.send(JSON.stringify(result.rows));
+    
+});
+});
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/test-db',function(req,res){
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
